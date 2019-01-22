682174f96349c9d750b647bc063935020f97a7c8
vinicius-guima <vinicius_guima.social@hotmail.com>
2019-01-22 10:56:58 -0200

    SpringData Criteribuilder

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,19 +1,15 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.input.TopicSearchInputDto;
 import br.com.alura.forum.controller.repository.TopicRepository;
-import br.com.alura.forum.model.Category;
-import br.com.alura.forum.model.Course;
-import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
 
 @RestController
@@ -23,17 +19,16 @@ public class TopicController {
 	private TopicRepository topicRepository;
 	
 	@GetMapping(value = "/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<TopicBriefOutPutDto> listTopics(){
+	public List<TopicBriefOutPutDto> listTopics(TopicSearchInputDto topicSearch){
 //		
 //		Category subCategory = new Category("java", new Category("Programação"));
 //		Course course = new Course("java e JSF", subCategory);
 //		Topic topic = new Topic("Problemas com JSF", "Erro ao fazer conversão de data", 
 //				new User("Fulano", "fulano@gmail.com", "123456"), course);
+	
+		Specification<Topic> topicSearchSpecification = topicSearch.build();
+		List <Topic> topics = topicRepository.findAll(topicSearchSpecification);
 		
-		
-		
-		
-		List <Topic> topics = topicRepository.findAll();
 		return TopicBriefOutPutDto.listFromTopics(topics);
 	}
 
